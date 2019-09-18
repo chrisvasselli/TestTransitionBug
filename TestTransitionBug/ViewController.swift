da78a4e6c382a285911b57c40bab57aea4fd006a
Wedge <clindsay+wedge@gmail.com>
2019-09-18 12:34:35 -0400

    Bug reproduction.

--- TestTransitionBug/ViewController.swift
+++ TestTransitionBug/ViewController.swift
@@ -14,7 +14,44 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destination = segue.destination
+        destination.transitioningDelegate = self
+    }
 
 
 }
+
+extension ViewController: UIViewControllerAnimatedTransitioning {
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.3
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        
+        if transitionContext.view(forKey: .from) == nil {
+            print("From view does NOT exist.")
+        }
+        else {
+            print("From view DOES exist.")
+        }
+        
+        
+        transitionContext.completeTransition(false)
+    }
+    
+    
+}
+
+extension ViewController: UIViewControllerTransitioningDelegate {
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return self
+    }
+    
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return self
+    }
+}
+
 
